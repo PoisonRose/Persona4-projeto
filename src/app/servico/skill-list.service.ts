89b26867e34a25bed28da02e8771a553aa54aecb
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2024-11-25 20:54:26 -0300

    added skills service

--- src/app/servico/skill-list.service.ts
+++ src/app/servico/skill-list.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillListService {
-
-  constructor() { }
+  getLink(link:string){
+    return this.apiLink = `http://localhost:3000/skills${link}`
+  }
+  constructor(private http: HttpClient) { }
+  apiLink:string = this.getLink('');
+  getAllSkills(){
+    return this.http.get(this.apiLink);
+  }
+  
 }
