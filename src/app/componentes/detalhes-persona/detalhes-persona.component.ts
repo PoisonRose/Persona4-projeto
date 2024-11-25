ecd5368fe065453a8068110610cd7a39e6071121
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2024-11-25 20:30:28 -0300

    started woking on detalhes

--- src/app/componentes/detalhes-persona/detalhes-persona.component.ts
+++ src/app/componentes/detalhes-persona/detalhes-persona.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CompendiumService } from '../../servico/compendium.service';
 
 @Component({
   selector: 'app-detalhes-persona',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './detalhes-persona.component.css'
 })
 export class DetalhesPersonaComponent {
+  constructor(private comp:CompendiumService){}
+
+  personas:any = '';
+  
+  ngOnInit(): void {
+      this.comp.getAllPersonas().subscribe((data: any)=>{
+          console.log(data);
+          this.personas = data;
+      });
+  }
 
 }
