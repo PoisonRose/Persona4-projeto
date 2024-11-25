40039781bd2b96b7e85b2b11e94990eb72253eb4
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2024-11-25 16:49:37 -0300

    implemented compendium service into perfil component

--- src/app/componentes/perfil/perfil.component.ts
+++ src/app/componentes/perfil/perfil.component.ts
@@ -1,11 +1,22 @@
-import { Component, input} from '@angular/core';
+import { Component, input, OnInit} from '@angular/core';
 import { HomeComponent } from '../../paginas/home/home.component';
+import { CompendiumService } from '../../servico/compendium.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css',
 })
-export class PerfilComponent{
+export class PerfilComponent implements OnInit{
+  constructor(private comp: CompendiumService){ }
 
+  personas:any = '';
+
+  ngOnInit(): void {
+    this.comp.getLink("?name=Mothman");
+    this.comp.getAllPersonas().subscribe((data: any)=>{
+      this.personas = data;
+      console.log(data);
+    });
+  }
 }
