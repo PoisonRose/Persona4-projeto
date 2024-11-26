1110b4ee1fae097ed1f6e2ff3d500d2402586d75
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2024-11-25 21:16:39 -0300

    started skill list

--- src/app/componentes/propriedades-skills/propriedades-skills.component.ts
+++ src/app/componentes/propriedades-skills/propriedades-skills.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SkillListService } from '../../servico/skill-list.service';
 
 @Component({
   selector: 'app-propriedades-skills',
   templateUrl: './propriedades-skills.component.html',
   styleUrl: './propriedades-skills.component.css'
 })
-export class PropriedadesSkillsComponent {
 
+export class PropriedadesSkillsComponent implements OnInit{
+  constructor(private skill: SkillListService){}
+
+  skills: any= "";
+
+  ngOnInit(): void {
+      this.skill.getLink('');
+      this.skill.getAllSkills().subscribe((data: any)=>{
+        console.log(data);
+        this.skills = data;
+      });
+  }
 }
